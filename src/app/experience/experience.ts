deea30363509bddf851ecdf809dcedc5a066f6a1
chefomar <elshinnaweyom@gmail.com>
2018-12-05 21:18:48 +0300

    update job title

--- src/app/experience/experience.ts
+++ src/app/experience/experience.ts
@@ -2,7 +2,7 @@ export const experience = [
   {
     from: 'Sept. 2017',
     to: 'Present',
-    title: 'Front-end Application Architect',
+    title: 'Application Architect',
     workplace: 'General Organization for Social Insurance'
   },
   {
