6b63ece25975f7ef27ca9774fb1c863634fe6f1b
chefomar <elshinnaweyom@gmail.com>
2018-12-10 19:35:22 +0300

    edit contact links for accessability

--- src/app/contacts/contacts.ts
+++ src/app/contacts/contacts.ts
@@ -1,27 +1,27 @@
 export const contacts = [
   {
     link: 'https://twitter.com/OmarAlShinnawey',
-    name: '@OmarAlshinnawey',
+    name: 'twitter',
     type: 'twitter'
   },
   {
     link: 'https://www.linkedin.com/in/omarelshinnawey',
-    name: '@omarelshinnawey',
+    name: 'linkedin',
     type: 'linkedin'
   },
   {
     link: 'https://github.com/chefomar',
-    name: '@chefomar',
+    name: 'github',
     type: 'github'
   },
   {
     link: 'https://stackoverflow.com/users/6152831/omar',
-    name: '@omar',
+    name: 'stackoverflow',
     type: 'stackoverflow'
   },
   {
     link: 'mailto:elshinnaweyom@gmail.com',
-    name: 'elshinnaweyom@gmail.com',
+    name: 'email',
     type: 'mail'
   }
 ];
